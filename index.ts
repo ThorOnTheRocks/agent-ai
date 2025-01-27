4d6876f5b13d96dbdb7a62365274fb625f64faca
gianlucagalota <gianluca.galota@gmail.com>
2025-01-27 08:27:39 +0100

    feat: added memory context

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { runLLM } from './src/llm';
+import { addMessages, getMessages } from './src/memory';
 
 const userMessage = process.argv[2];
 
@@ -7,9 +8,13 @@ if (!userMessage) {
   console.log('Please provide a message');
   process.exit(1);
 }
+await addMessages([{ role: 'user', content: userMessage }]);
+const messages = await getMessages();
 
 const response = await runLLM({
-  messages: [{ role: 'user', content: userMessage }],
+  messages: [...messages, { role: 'user', content: userMessage }],
 });
+
+await addMessages([{ role: 'assistant', content: response }]);
 
 console.log(response);
--- src/llm.ts
+++ src/llm.ts
@@ -2,18 +2,14 @@ import type { AIMessage } from '../types';
 import { openai } from './ai';
 
 export const runLLM = async ({
-  model = 'gpt-4o-mini',
   messages,
-  temperature = 0.1,
 }: {
   messages: AIMessage[];
-  model?: string;
-  temperature?: number;
 }) => {
   const response = await openai.chat.completions.create({
-    model,
+    model: 'gpt-4o-mini',
+    temperature: 0.1,
     messages,
-    temperature,
   });
 
   return response.choices[0].message.content;
